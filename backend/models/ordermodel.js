afa98f0ca3cce57f9325a3111f4c425b8e327fd2
Levi Graham <GrahamLeviW@gmail.com>
2020-12-02 11:38:26 -0500

    Created mongoose schemas

---
+++ backend/models/ordermodel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product',
+        },
+      },
+    ],
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product
