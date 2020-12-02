36c3930c0935549430f772b5213c1ccc233b56a0
Levi Graham <GrahamLeviW@gmail.com>
2020-12-02 11:13:03 -0500

    Set up mongoose DB file, added colors package

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 /* Entry point for backend */
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
-
+connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
@@ -24,5 +26,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`Server running on ${PORT} in ${process.env.NODE_ENV}`)
+  console.log(
+    `Server running on ${PORT} in ${process.env.NODE_ENV}`.yellow.bold
+  )
 )
