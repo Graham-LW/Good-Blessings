6466ff7801fad584cf66b9841d665c2448767e9d
Levi Graham <GrahamLeviW@gmail.com>
2020-12-04 08:05:07 -0500

    seeder.js works, imports and deletes database. Broke products becuause local id field was removed for database id

---
+++ backend/seeder.js
@@ -0,0 +1,66 @@
+/* Seperate script to clear/import into the database */
+
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import users from './data/users.js'
+import products from './data/products.js'
+import User from './models/usermodel.js'
+import Product from './models/productmodel.js'
+import Order from './models/ordermodel.js'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+connectDB()
+
+const importData = async () => {
+  try {
+    /* Promise to delete database entries in our lists */
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    /* CreatedUsers set equal to the new list of users, admin is first list in user(based on this implementation) */
+    const createdUsers = await User.insertMany(users)
+    const adminUser = createdUsers[0]._id
+
+    /* sampleProducts set equal to procuts passed in, plus a field user, set to our admin user above */
+    const sampleProducts = products.map((product) => {
+      return {
+        ...product,
+        user: adminUser,
+      }
+    })
+
+    /* Insert Products with admin user field attached */
+    await Product.insertMany(sampleProducts)
+    console.log('Data Imported'.green.inverse)
+    process.exit()
+  } catch (error) {
+    console.log(`${error}`.red.inverse)
+    process.exit(1)
+  }
+}
+
+const destroyData = async () => {
+  try {
+    /* Promise to delete database entries */
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
+
+    console.log('Data Destroyed'.red.inverse)
+    process.exit()
+  } catch (error) {
+    console.log(`${error}`.red.inverse)
+    process.exit(1)
+  }
+}
+
+/* Process.argv is commandline argument, if -d flag is typed, queue datadestroy, otherwise run script with update. */
+if (process.argv[2] === '-d') {
+  destroyData()
+} else {
+  importData()
+}
